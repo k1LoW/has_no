40ba43d769a403b6eee25712e3b68fb8ca37d5a2
k1LoW <k1lowxb@gmail.com>
2010-04-19 11:36:05 +0900

    Add license.

--- has_no_app_model.php
+++ has_no_app_model.php
@@ -1,10 +1,15 @@
 <?php
 
   /**
-   * CakePHP HasNo Plugin
+   * HasNo: hasNo() plugin for CakePHP.
    *
+   */
+  /**
+   * HasNoAppModel code license:
    *
-   *
+   * @copyright   Copyright (C) 2010 by 101000code/101000LAB
+   * @since       CakePHP(tm) v 1.3
+   * @license     http://www.opensource.org/licenses/mit-license.php The MIT License
    */
 class HasNoAppModel extends AppModel {
   }
